bf336a9882b3ab0dc9d8cab293b4121c6bb5ac6c
Eddie Liu <liuedw@uw.edu>
2020-11-29 21:14:53 -0800

    Updated to include home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
-import ParticipantPage from "./components/participant/ParticipantPage";
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch, useParams } from 'react-router-dom'
+import './App.css'
+import ParticipantPage from "./components/participant/ParticipantPage"
+import ProjectPage from "./components/project/ProjectPage"
 
 class App extends Component {
+
   render() {
     return (
-        <ParticipantPage projectID={0} />
-    );
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/" children={<ProjectPage />} exact/>
+            <Route path="/projectID=:id" children={<Child />} />
+          </Switch>
+        </div> 
+      </BrowserRouter>
+    )
   }
+}
+
+function Child() {
+  let { id } = useParams();
+
+  return (
+    <ParticipantPage projectID={id} />
+  );
 }
 
 export default App;
